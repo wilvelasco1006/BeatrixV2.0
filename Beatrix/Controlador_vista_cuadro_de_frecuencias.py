317a0e7381e19021b322b486d44abfb92e07d604
AndresPort <andresfelipe29portillo@gmail.com>
2024-03-30 15:53:24 -0500

    Se agregó el método ingresar_y_almacenar_todos_los_datos en la  clase Controlador_vista_cuadro_de_frecuencias para pedirle al usuario datos y almacenarlos en una lista

--- Beatrix/Controlador_vista_cuadro_de_frecuencias.py
+++ Beatrix/Controlador_vista_cuadro_de_frecuencias.py
@@ -1,8 +1,22 @@
 from Cuadro_de_frecuencias import Cuadro_de_frecuencias_datos_individuales as cuadro_de_frecuencias
 
 class Controlador_vista_cuadro_de_frecuencias:
+    #Atributos
+    un_cuadro_de_frecuencias= cuadro_de_frecuencias()
     
     #Metodos
     #Metodo constructor
     def __init__(self):
         pass
+    
+    def ingresar_y_almacenar_todos_los_datos(self):
+        continuar= 1
+        todos_los_datos= list()
+        
+        while(continuar== 1):
+            dato=input("Por favor ingrese los datos que desee analizar:  ")
+            todos_los_datos.append(dato)
+            continuar= int(input("Desea agregar otro dato? 1.Si  2.No\n\n"))
+            
+        print( todos_los_datos)
+
